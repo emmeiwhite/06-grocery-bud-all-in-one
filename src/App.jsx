6b2d0933337227613f31c0f0834968f7f0d037b0
emmeiwhite <emmeiwhite@gmail.com>
2024-10-20 16:38:16 +0530

    Form Submit: Creating List of Array Objects done!

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
-const App = () => {
-  return <h2>Grocery Bud - Starter</h2>;
-};
+import { useState } from 'react'
+import GroceryForm from './components/GroceryForm'
 
-export default App;
+const App = () => {
+  const [itemList, setItemList] = useState([])
+
+  function getItem(groceryItem) {
+    console.log(groceryItem)
+    setItemList([...itemList, groceryItem])
+    console.log(itemList)
+  }
+  return (
+    <main className="section-center">
+      <GroceryForm getItem={getItem} />
+    </main>
+  )
+}
+
+export default App
--- src/components/GroceryForm.jsx
+++ src/components/GroceryForm.jsx
@@ -1,3 +1,35 @@
-export default function GroceryForm() {
-  return <div></div>
+import { useState } from 'react'
+import { nanoid } from 'nanoid'
+
+export default function GroceryForm({ getItem }) {
+  const [item, setItem] = useState('')
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    if (item) {
+      const itemObj = {
+        id: nanoid(),
+        name: item,
+        isChecked: false
+      }
+      getItem(itemObj)
+
+      setItem('')
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form-row">
+        <input
+          type="text"
+          onChange={e => setItem(e.target.value)}
+          value={item}
+          className="form-input"
+        />
+        <button className="btn">Add Item</button>
+      </div>
+    </form>
+  )
 }
